3e6bb2b5168c2189a2611438e740feddfe1e9923
Raimbek <rrakhimbekov@gmail.com>
2023-11-11 22:26:30 +0600

    documentation

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import uvicorn
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
+from stations import find_all_stations
 from stations import find_paths
 from stations import find_stations
-from stations import find_all_stations
 from trains import find_all_trains_at_station
 
 app = FastAPI(
@@ -31,21 +31,45 @@ async def root():
 
 @app.get("/api/stations/path")
 async def stations_path(start=134, end=155, candidates_constraint=3):
+    """
+Возможные пути от стартовой до конечной станции
+    :param start: стартовая станция
+    :param end: конечная станция
+    :param candidates_constraint: максимальное количество кандидатов
+    :return:
+    """
     return find_paths(int(start), int(end), int(candidates_constraint))
 
 
 @app.get("/api/stations/net")
 async def stations(left, top, right, down):
+    """
+Все станции в данном квадрате
+    :param left:
+    :param top:
+    :param right:
+    :param down:
+    :return:
+    """
     return find_stations(float(left), float(top), float(right), float(down))
 
 
 @app.get("/api/stations")
 async def stations():
+    """
+Все возможные станции
+    :return: все возможный станции
+    """
     return find_all_stations()
 
 
 @app.get("/api/trains")
 async def stations(station):
+    """
+Все поезда на данной станции
+    :param station: номер станций
+    :return: все поезда на данной станций
+    """
     return find_all_trains_at_station(int(station))
 
 
