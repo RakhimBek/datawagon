fb0ebf4bcb8ba916ef1dd772dc6c91621ab52e6d
Raimbek <rrakhimbekov@gmail.com>
2023-11-12 09:28:15 +0600

    dislocations sql helper file

--- helpers.sql
+++ helpers.sql
@@ -1,4 +1,5 @@
 -- remove all dislocations hardly associated with the current set of STATIONS
-select count(*) from DISLOCATIONS where STDISL not in (select ID from STATIONS)
+select count(*) from DISLOCATIONS where STDISL not in (select ID from STATIONS);
 
-
+-- remove all unsuited rows
+delete from DISLOCATIONS where TRAININDEX not like '%-%';
